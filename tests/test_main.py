8fd5147541b97fc6626f55f8b17911fa1e170862
shimamori eiki <44014265+shimamorieiki@users.noreply.github.com>
2025-02-22 13:45:07 +0900

    Update test_main.py

--- tests/test_main.py
+++ tests/test_main.py
@@ -10,12 +10,12 @@ def test_add():
   assert add("3", "4", "5") == 39
   assert add(2.6, "4", None) == "error"
   assert add(2.5, 3, "5.1") == "error"
+  assert add(5.4, 9.8) == 15
   assert add("1.4", "2", 3) == "error"
   assert add(2.5, 3, "x") == "error"
   assert add(2.1, 9.3, 3.7) == 14
   assert add(1, 6, 3) == 10
-  assert add("a", None) == "error"
-  assert add(1.6, 2.5, 3.7) == 7
+  assert add(1.6, 2.1) == 3
   assert add(3.1, 2) == 5
   assert add(1.9, 4.3, "2.6") == "error"
   assert add("1", "2", 9) == 21
